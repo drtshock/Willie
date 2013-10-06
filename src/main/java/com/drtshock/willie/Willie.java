c78c9d4540ea4b84dd577436bca036004c034d1b
3PlayingGames <3playgames99@gmail.com>
2013-10-06 09:13:38 -0700

    Update Willie.java
    Added slap Command and imported

--- src/main/java/com/drtshock/willie/Willie.java
+++ src/main/java/com/drtshock/willie/Willie.java
@@ -34,6 +34,7 @@ import com.drtshock.willie.command.fun.PopcornCommandHandler;
 import com.drtshock.willie.command.fun.TWSSCommandHandler;
 import com.drtshock.willie.command.fun.UrbanCommandHandler;
 import com.drtshock.willie.command.fun.WhipCommandHandler;
+import com.drtshock.willie.command.fun.SlapCommandHandler;
 import com.drtshock.willie.command.management.KickCommandHandler;
 import com.drtshock.willie.command.minecraft.GlobalMCStatsCommandHandler;
 import com.drtshock.willie.command.minecraft.MCStatsCommandHandler;
@@ -175,7 +176,9 @@ public class Willie extends PircBotX {
         this.commandManager.registerCommand(new Command("gstats", "[auth] - Global MCStats stats", new GlobalMCStatsCommandHandler()));
         this.commandManager.registerCommand(new Command("xkcd", "<nb> - Get an xkcd", new XKCDCommandHandler()));
         this.commandManager.registerCommand(new Command("botsnack", "feed the bot!", new BotSnacksCommandHandler()));
-
+        this.commandManager.registerCommand(new Command("Slap", "Slap them around!", new SlapCommandHandler()));
+        
+        
         this.commandManager.registerCommand(new Command("join", "<channel> - Joins a channel", new JoinCommandHandler(), true));
         this.commandManager.registerCommand(new Command("shutdown", "shuts the bot down", new ShutdownCommandHandler(), true));
         this.commandManager.registerCommand(new Command("leave", "<channel> - Leaves a channel", new LeaveCommandHandler(), true));
