024dfacae723d53903831db2d86aa5db3ff2fb42
3PlayingGames <3playgames99@gmail.com>
2013-10-06 01:53:29 -0700

    Create DinnerTimeCommandHandler.java
    New Fun command :)

---
+++ src/main/java/com/drtshock/willie/command/fun/DinnerTimeCommandHandler.java
@@ -0,0 +1,15 @@
+package com.drtshock.willie.command.fun;
+
+import org.pircbotx.Channel;
+import org.pircbotx.User;
+
+import com.drtshock.willie.Willie;
+import com.drtshock.willie.command.CommandHandler;
+
+public class DinnerTimeCommandHandler implements CommandHandler {
+
+    @Override
+    public void handle(Willie bot, Channel channel, User sender, String[] args) {
+        bot.sendAction(channel, "ITS DINNERTIME!!!!!");
+    }
+}
