5d8b471dfc4c5d33022f72696e1b56b080b8ea71
Luke (stuntguy3000) <stuntguy3000@gmail.com>
2013-11-10 12:05:06 +1030

    Lets debug !wdtbs

--- src/main/java/com/drtshock/willie/command/fun/WDTBSCommandHandler.java
+++ src/main/java/com/drtshock/willie/command/fun/WDTBSCommandHandler.java
@@ -6,6 +6,7 @@ import com.google.code.chatterbotapi.ChatterBot;
 import com.google.code.chatterbotapi.ChatterBotFactory;
 import com.google.code.chatterbotapi.ChatterBotSession;
 import com.google.code.chatterbotapi.ChatterBotType;
+
 import org.pircbotx.Channel;
 import org.pircbotx.Colors;
 import org.pircbotx.User;
@@ -16,30 +17,39 @@ import org.pircbotx.User;
 public class WDTBSCommandHandler implements CommandHandler {
 
     @Override
-    public void handle(Willie bot, Channel channel, User sender, String[] args) throws Exception {
+    public void handle(Willie bot, Channel channel, User sender, String[] args) {
         if (args.length < 1) {
             channel.sendMessage(Colors.RED + "Please supply a message " + sender.getNick() + "!");
             return;
         }
 
-        ChatterBotFactory factory = new ChatterBotFactory();
-
-        ChatterBot cbot = factory.create(ChatterBotType.CLEVERBOT);
-        ChatterBotSession cBotSession = cbot.createSession();
-
-        ChatterBot pbot = factory.create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477");
-        ChatterBotSession pBotSession = pbot.createSession();
-
-        StringBuilder question = new StringBuilder();
-        for (String arg : args) {
-            question.append(arg + " ");
+        try {
+            ChatterBotFactory factory = new ChatterBotFactory();
+    
+            ChatterBot cbot;
+            
+            cbot = factory.create(ChatterBotType.CLEVERBOT);
+            
+            ChatterBotSession cBotSession = cbot.createSession();
+    
+            ChatterBot pbot = factory.create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477");
+            ChatterBotSession pBotSession = pbot.createSession();
+    
+            StringBuilder question = new StringBuilder();
+            for (String arg : args) {
+                question.append(arg + " ");
+            }
+    
+            String cBotResponce = cBotSession.think(question.toString());
+            String pBotResponce = pBotSession.think(question.toString());
+    
+            channel.sendMessage(Colors.BROWN + "What does the bot say?");
+            channel.sendMessage(Colors.CYAN + "Cleverbot: " + Colors.DARK_GREEN + cBotResponce);
+            channel.sendMessage(Colors.CYAN + "Pandorabot: " + Colors.DARK_GREEN + pBotResponce);
+        
+        } catch (Exception e) {
+            channel.sendMessage(Colors.RED + "An error has occured: " + e.getMessage());
+            e.printStackTrace();
         }
-
-        String cBotResponce = cBotSession.think(question.toString());
-        String pBotResponce = pBotSession.think(question.toString());
-
-        channel.sendMessage(Colors.BROWN + "What does the bot say?");
-        channel.sendMessage(Colors.CYAN + "Cleverbot: " + Colors.DARK_GREEN + cBotResponce);
-        channel.sendMessage(Colors.CYAN + "Pandorabot: " + Colors.DARK_GREEN + pBotResponce);
     }
 }
