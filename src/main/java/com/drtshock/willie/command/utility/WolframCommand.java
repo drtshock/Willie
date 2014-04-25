526a974865c64658b5b53d62218054f52db2012c
hatstand <hatstand0@gmail.com>
2014-04-26 06:57:45 +1000

    We Wolfram Alpha now.

---
+++ src/main/java/com/drtshock/willie/command/utility/WolframCommand.java
@@ -0,0 +1,66 @@
+package com.drtshock.willie.command.utility;
+
+import com.drtshock.willie.Willie;
+import com.drtshock.willie.command.CommandHandler;
+import com.drtshock.willie.util.WebHelper;
+import javafx.scene.paint.Color;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+import org.pircbotx.Channel;
+import org.pircbotx.User;
+
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class WolframCommand implements CommandHandler {
+
+    private static final String API_URL = "http://api.wolframalpha.com/v2/query?format=plaintext&appid=%s&input=%s",
+    QUERY_URL = "http://www.wolframalpha.com/input/?i=%s";
+
+    private SAXBuilder builder = new SAXBuilder();
+
+    @Override
+    public void handle(Willie bot, Channel channel, User sender, String[] args) throws Exception {
+        if(args.length == 0) {
+            channel.sendMessage(Color.RED + "Usage: wa <query>");
+        }
+        else {
+            String key = bot.getConfig().getWolframApiKey();
+
+            if (key.equals("change-me")) {
+                throw new IllegalStateException("Missing API key");
+            } else {
+                StringBuilder argsBuilder = new StringBuilder();
+
+                for(String arg : args) {
+                    argsBuilder.append(arg);
+
+                    if(arg != args[args.length - 1]) {
+                        argsBuilder.append(" ");
+                    }
+                }
+
+                String input = URLEncoder.encode(argsBuilder.toString(), "UTF-8");
+
+                Document document = builder.build(new URL(String.format(API_URL, key, input)));
+
+                StringBuilder answer = new StringBuilder();
+
+                for(Element pod : document.getRootElement().getChildren("pod")) {
+                    if(pod.getAttribute("primary") != null && pod.getAttribute("primary").getBooleanValue()) {
+                        for(Element subPod : pod.getChildren("subpod")) {
+                            String content = subPod.getChild("plaintext").getText().replaceAll("\\n", "; ").replaceAll("\\s+", " ");
+
+                            if(!content.isEmpty()) {
+                                answer.append(content);
+                            }
+                        }
+                    }
+                }
+
+                channel.sendMessage(String.format("%s - %s", answer, WebHelper.shortenURL(String.format(QUERY_URL, input))));
+            }
+        }
+    }
+}
